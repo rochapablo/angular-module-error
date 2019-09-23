0fbf312ea3e288b83552811e705d13bcc31928e5
Pablo Rocha <rocha.pabloricardo@gmail.com>
2019-09-23 07:24:30 -0300

    testing

--- my-dream-app2/src/app/app.module.ts
+++ my-dream-app2/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { PabloPaginationModule } from 'my-dream-app/dist/pablo-pagination';
+import { PabloPaginationModule } from 'pablo-pagination';
 
 @NgModule({
   declarations: [
